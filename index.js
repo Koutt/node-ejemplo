25386bcd6cc5baf43e73bde2009c66a7192d4e83
Angel Arteaga <angel@smarty-web.net>
2023-08-23 12:19:12 -0600

    nueva funcion

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-console.log('Hola!!!');
+/* Ejemplo de un módulo Javascript mediante una función de auto-llamado.
+IIFE - Immediately Invoked Function Expression)
+*/
+(() => {
+
+    function metodoPrivado1(parametro){
+    
+    }
+
+    function metodoPrivado2(parametro){
+ 
+    }
+
+    exports.MetodoPublico = metodoPrivado2;
+    
+ })();
+ 
